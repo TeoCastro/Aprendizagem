d46ff5cb84996f1d7bccf8b35150e17ae32be6ed
TeoCastro <62861504+TeoCastro@users.noreply.github.com>
2020-06-29 13:46:16 -0300

    Create Primeiro.py
    Mudanças para pandas, kivy e outros

---
+++ Tkinter/Primeiro.py
@@ -0,0 +1,14 @@
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+from  tkinter import *
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+tela_inicial = Tk()
+tela_inicial.title('Janela 1')
+tela_inicial.mainloop()
