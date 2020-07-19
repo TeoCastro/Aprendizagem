e14c11e3f94384e6295918c7fd808f3a31cfcd14
TeoCastro <62861504+TeoCastro@users.noreply.github.com>
2020-07-18 21:05:21 -0300

    Exibindo por coluna

--- Tkinter/PrimeiroPandas.py
+++ Tkinter/PrimeiroPandas.py
@@ -5,15 +5,28 @@ from tkinter import *
 def prijanela():
     tela_inicial = Tk()
     tela_inicial.title('Janela 1')
-    Label(tela_inicial, text=tabclientes3['SEXO'], justify=LEFT).pack()
+# primeira Label
+    texto = Label(tela_inicial, text=nome, width= 40, justify=LEFT, bg="red")
+    texto.pack(side=LEFT, fill=BOTH, expand=1)
+# segunda Label
+    texto1 = Label(tela_inicial, text=sexo, width= 15, justify=LEFT, bg="yellow")
+    texto1.pack(side=LEFT, fill=BOTH, expand=1)
+# terceira Label
+    texto2 = Label(tela_inicial, text=cpf, width=20, justify=LEFT, bg="blue")
+    texto2.pack(side=LEFT, fill=BOTH, expand=1)
+
     tela_inicial.mainloop()
-    
+
+
+
+
+
 clientes = pd.read_excel('C:/Users/siuit/Documents/Python/PLANILHA_CLIENTES.XLSX')
-
 tabclientes = pd.DataFrame(clientes)
+sexo = clientes['SEXO']
+nome = clientes['NOME']
+cpf = clientes['CPF']
 
-tabclientes3 = tabclientes.loc[tabclientes['SEXO']== 'F']
-print (tabclientes3)
+
 
 prijanela()
-
