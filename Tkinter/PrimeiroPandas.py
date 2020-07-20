429fbed82d568782f1064b987901305e177245e3
TeoCastro <62861504+TeoCastro@users.noreply.github.com>
2020-07-20 16:08:46 -0300

    Substituindo dados faltantes

--- Tkinter/PrimeiroPandas.py
+++ Tkinter/PrimeiroPandas.py
@@ -5,19 +5,18 @@ from tkinter import *
 def prijanela():
     tela_inicial = Tk()
     tela_inicial.title('Janela 1')
-# primeira Label
-    texto = Label(tela_inicial, text=nome, width= 40, justify=LEFT, bg="red")
-    texto.pack(side=LEFT, fill=BOTH, expand=1)
-# segunda Label
-    texto1 = Label(tela_inicial, text=sexo, width= 15, justify=LEFT, bg="yellow")
-    texto1.pack(side=LEFT, fill=BOTH, expand=1)
-# terceira Label
-    texto2 = Label(tela_inicial, text=cpf, width=20, justify=LEFT, bg="blue")
-    texto2.pack(side=LEFT, fill=BOTH, expand=1)
+# criando os Label
 
-# quarto Label
+    texto = Label(tela_inicial, text=nome, width= 40, justify=LEFT, bg="red")
+    texto1 = Label(tela_inicial, text=sexo, width= 15, justify=LEFT, bg="yellow")
     texto2 = Label(tela_inicial, text=bairro, width=20, justify=LEFT, bg="pink")
+    texto3 = Label(tela_inicial, text=cpf, width=20, justify=LEFT, bg="blue")
+
+# mostrando os Label
+    texto.pack(side=LEFT, fill=BOTH, expand=1)
+    texto1.pack(side=LEFT, fill=BOTH, expand=1)
     texto2.pack(side=LEFT, fill=BOTH, expand=1)
+    texto3.pack(side=LEFT, fill=BOTH, expand=1)
 
     tela_inicial.mainloop()
 
@@ -27,10 +26,11 @@ def prijanela():
 
 clientes = pd.read_excel('C:/Users/siuit/Documents/Python/PLANILHA_CLIENTES.XLSX')
 tabclientes = pd.DataFrame(clientes)
-sexo = clientes['SEXO']
-nome = clientes['NOME']
-cpf = clientes['CPF']
-bairro = clientes['BAIRRO']
+
+sexo = clientes['SEXO'].fillna(' ')
+nome = clientes['NOME'].fillna(' ')
+cpf = clientes['CPF'].fillna(' ')
+bairro = clientes['BAIRRO'].fillna(' ')
 
 
 
