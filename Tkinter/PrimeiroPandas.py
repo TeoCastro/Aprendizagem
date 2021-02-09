16f015a442ae2739b02acdd2a79e87256195d49b
TeoCastro <62861504+TeoCastro@users.noreply.github.com>
2021-02-09 14:44:13 -0300

    Update PrimeiroPandas.py

--- Tkinter/PrimeiroPandas.py
+++ Tkinter/PrimeiroPandas.py
@@ -3,9 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tkinter import *
 
+
+
 def prijanela():
     tela_inicial = Tk()
     tela_inicial.title('Janela 1')
+    lista = Listbox(tela_inicial)
+    
 # criando os Label
 
     texto = Label(tela_inicial, text=nome, width= 40, justify=LEFT, bg="red")
@@ -15,7 +19,8 @@ def prijanela():
     texto4 = Label(tela_inicial, text=telefone, width=20, justify=LEFT, bg="gray")
     texto5 = Label(tela_inicial, text=datanasc, width=20, justify=LEFT, bg="brown")
     texto6 = Label(tela_inicial, text=queixainicial, width=20, justify=LEFT, bg="beige")
-
+    lista.insert(0, texto)
+    lista.insert(0, Label)
 
 
 # mostrando os Label
@@ -26,7 +31,7 @@ def prijanela():
     texto4.pack(side=LEFT, fill=BOTH, expand=1)
     texto5.pack(side=LEFT, fill=BOTH, expand=1)
     texto6.pack(side=LEFT, fill=BOTH, expand=1)    
-
+    lista.pack()
 
     tela_inicial.mainloop()
 
@@ -36,6 +41,8 @@ def prijanela():
 
 clientes = pd.read_excel('C:/Users/siuit/Documents/Python/PLANILHA_CLIENTES.XLSX')
 tabclientes = pd.DataFrame(clientes)
+#print(tabclientes)
+tabclientes.head()
 
 sexo = clientes['SEXO'].fillna(' ')
 nome = clientes['NOME'].fillna(' ')
@@ -44,9 +51,9 @@ bairro = clientes['BAIRRO'].fillna(' ')
 telefone = clientes['TELEFONE'].fillna(' ')
 datanasc = clientes['DATA DE NASC'].fillna(' ')
 queixainicial = clientes['QUEIXA INICIAL'].fillna(' ')
-plt.plot(clientes['TELEFONE'])
-plt.show()
-plt.pie(clientes['TELEFONE'])
-plt.show()
+#plt.plot(clientes['TELEFONE'])
+#plt.show()
+#plt.pie(clientes['TELEFONE'])
+#plt.show()
 
 prijanela()
