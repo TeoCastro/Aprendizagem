86a4b9c43bd6278c01addbb7c3f53f36a1eb21c3
TeoCastro <62861504+TeoCastro@users.noreply.github.com>
2021-02-19 17:57:23 -0300

    Carregando dados no banco de dados

--- Tkinter/Primeiro.py
+++ Tkinter/Primeiro.py
@@ -9,17 +9,39 @@ def impdados():
     print('Oi')
     final.set( edit_inicial.get())
 
+def insdados():
+    inicial1 = edit_inicial.get()
+    primeiro1 = edit_primeiro.get()
+    idade1 = edit_idade.get()
+    dataInicio = edit_data.get()
+    telefone1 = edit_telefone.get()
+    hipotese1 = edit_hipotese.get()
+    outros1 = edit_outros.get()
+
+    cursor.execute('INSERT INTO clientes(NOME,IDADE,DATA,TELEFONE, HIPOTESE,PRIMEIRO,OUTROS)VALUES(?,?,?,?,?,?,?)',(inicial1,idade1,dataInicio,telefone1, hipotese1,primeiro1,outros1))
+
+
+
+
 
 #=================== CRIANDO E APRESENTANDO A JANELA PRINCIPAL ==================
 
 tela_inicial = Tk()
 tela_inicial.title('Janela Principal')
-tela_inicial.geometry("600x500")
+tela_inicial.geometry("1150x700")
 
 
 #==================== CARREGANDO PLANILHA DE CLIENTES DO EXCEL ===================
 
-clientes = pd.read_excel('C:/Users/siuit/Documents/Python/PLANILHA_CLIENTES.XLSX')
+#clientes = pd.read_excel('C:/Users/siuit/Documents/Python/Aprendizagem/Clientes do consultório.xlsx')
+
+#nome = clientes['NOME'].fillna(' ')
+#idade_clientes = clientes['IDADE'].fillna(' ')
+#sexo = clientes['SEXO'].fillna(' ')
+#telefone = clientes['TELEFONE'].fillna(' ')
+#bairro = clientes['BAIRRO'].fillna(' ')
+#queixainicial = clientes['QUEIXA INICIAL'].fillna(' ')
+
 
 
 #==================== CRIANDO BANCO DE DADOS E TABELA ============================
@@ -38,6 +60,8 @@ final.set('NOME')
 lcadastro = Label(tela_inicial, text='CADASTRO DE CLIENTES')
 label_inicial = Label(tela_inicial, textvariable=final)
 lidade = Label(tela_inicial,text='IDADE')
+ldata_inicio = Label(tela_inicial, text='DATA DE INICIO')
+ltelefone = Label(tela_inicial, text='TELEFONE')
 lhipotese = Label(tela_inicial, text='HIPÓTESE DIAGNÓSTICA')
 lprimeiro= Label(tela_inicial, text='PRIMEIRO ATENDIMENTO')
 loutros= Label(tela_inicial, text='OUTROS ATENDIMENTOS')
@@ -45,11 +69,14 @@ loutros= Label(tela_inicial, text='OUTROS ATENDIMENTOS')
 #===================== CRIANDO CAIXA DE TEXTO ===============================
 
 
-edit_primeiro=Text()
-edit_hipotese=Entry()
-edit_idade=Entry()
 edit_inicial = Entry()
+edit_idade=Entry()
+edit_telefone = Entry()
+edit_data = Entry()
+edit_hipotese=Entry()
 edit_outros = Text()
+edit_primeiro=Text()
+
 
 #===================== CRIANDO BOTÃO ========================================
 
@@ -61,23 +88,27 @@ botao_inicial = Button(tela_inicial,text="Imprimir", command=impdados)
 
 
 
-lcadastro.place(x=240, y=20)
-label_inicial.place(x=19, y=40)
-lidade.place(x=19, y=100)
-lhipotese.place(x=19, y=160)
-lprimeiro.place(x=19, y=220)
-loutros.place(x=19, y=360)
+lcadastro.place(x=490, y=10)
+label_inicial.place(x=19, y=60)
+lidade.place(x=530, y=60)
+ldata_inicio.place(x=930,y=60)
+ltelefone.place(x=710, y=60)
+lhipotese.place(x=490, y=160)
+lprimeiro.place(x=19, y=250)
+loutros.place(x=19, y=450)
 
 
 #====================== MOSTRANDO CAIXA DE TEXTO E BOTÃO ==================
 
 
-botao_inicial.place(x=390, y=270)
-edit_inicial.place(x=20, y=60, width=400)
-edit_idade.place(x=20, y=120, width=30)
-edit_hipotese.place(x=20, y=180, width=550)
-edit_primeiro.place(x=20, y=240, width=300, height= 100)
-edit_outros.place(x=20, y=380, width=300, height=100)
+botao_inicial.place(x=510, y=630, width=100, height=40)
+edit_inicial.place(x=20, y=80, width=400)
+edit_idade.place(x=533, y=80, width=30)
+edit_hipotese.place(x=100, y=180, width=930)
+edit_primeiro.place(x=20, y=270, width=1100, height= 150)
+edit_outros.place(x=20, y=470, width=1100, height=150)
+edit_data.place(x=933, y=80)
+edit_telefone.place(x=717, y=80)
 
 
 #====================== MOSTRANDO JANELA PRINCIPAL =======================
--- Tkinter/PrimeiroPandas.py
+++ Tkinter/PrimeiroPandas.py
@@ -11,31 +11,39 @@ def prijanela():
     tela_inicial = Tk()
     tela_inicial.title('Janela 1')
     lista = Listbox(tela_inicial)
-    
-# criando os Label
-
-    texto = Label(tela_inicial, text=nome, width= 40, justify=LEFT, bg="red")
-    texto1 = Label(tela_inicial, text=sexo, width= 15, justify=LEFT, bg="yellow")
-    texto2 = Label(tela_inicial, text=bairro, width=20, justify=LEFT, bg="pink")
-    texto3 = Label(tela_inicial, text=cpf, width=20, justify=LEFT, bg="blue")
-    texto4 = Label(tela_inicial, text=telefone, width=20, justify=LEFT, bg="gray")
-    texto5 = Label(tela_inicial, text=datanasc, width=20, justify=LEFT, bg="brown")
-    texto6 = Label(tela_inicial, text=queixainicial, width=20, justify=LEFT, bg="beige")
-    lista.insert(0, texto)
-    lista.insert(0, Label)
 
 
-# mostrando os Label
+def mostrar():
     texto.pack(side=LEFT, fill=BOTH, expand=1)
     texto1.pack(side=LEFT, fill=BOTH, expand=1)
     texto2.pack(side=LEFT, fill=BOTH, expand=1)
     texto3.pack(side=LEFT, fill=BOTH, expand=1)
     texto4.pack(side=LEFT, fill=BOTH, expand=1)
     texto5.pack(side=LEFT, fill=BOTH, expand=1)
-    texto6.pack(side=LEFT, fill=BOTH, expand=1)    
+    texto6.pack(side=LEFT, fill=BOTH, expand=1)
     lista.pack()
 
-    tela_inicial.mainloop()
+
+# prijanela()
+    
+# criando os Label
+
+texto = Label(tela_inicial, text=nome, width= 40, justify=LEFT, bg="red")
+texto1 = Label(tela_inicial, text=sexo, width= 15, justify=LEFT, bg="yellow")
+texto2 = Label(tela_inicial, text=bairro, width=20, justify=LEFT, bg="pink")
+texto3 = Label(tela_inicial, text=cpf, width=20, justify=LEFT, bg="blue")
+texto4 = Label(tela_inicial, text=telefone, width=20, justify=LEFT, bg="gray")
+texto5 = Label(tela_inicial, text=datanasc, width=20, justify=LEFT, bg="brown")
+texto6 = Label(tela_inicial, text=queixainicial, width=20, justify=LEFT, bg="beige")
+lista.insert(0, texto)
+lista.insert(0, Label)
+
+
+# mostrando os Label
+mostrar()
+
+
+tela_inicial.mainloop()
 
 
 
