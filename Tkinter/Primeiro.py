360661698fa9c48164188c813247a9a2ee16cd4c
TeoCastro <62861504+TeoCastro@users.noreply.github.com>
2021-03-23 06:36:29 -0300

    Criação da janela inserir

--- Tkinter/Primeiro.py
+++ Tkinter/Primeiro.py
@@ -1,82 +1,169 @@
-
-
-from tkinter import *
-import pandas as pd
 import sqlite3
+from tkinter import *
+
+''''==============================================================================================================
+=============================================== FUNÇÕES ======================================================
+=============================================================================================================='''
+
 
 def impdados():
     edit_outros.pack_forget()
 
+
 def nova_janela():
-    # ========== CRIANDO NOVA JANELA PARA MOSTRAR OS REGISTROS ============
-    
+    # ============================== CRIANDO NOVA JANELA PARA MOSTRAR OS REGISTROS ================================
+
     top = Toplevel()
+    top.title('Janela Busca')
     lb_registro = Listbox(top)
-    lb_registro.place(x=10, y=10)
-    cursor.execute( 'SELECT * FROM clientes' )
+    lb_registro.place(x=10, y=10, width=600, height=450)
+    top.geometry("800x500")
+    cursor.execute('SELECT * FROM clientes')
     pesq = cursor.fetchall()
-    print( pesq )
+    print(pesq)
     print("Número total de registros retornados: ", cursor.rowcount)
     for linha in pesq:
         print("Nome:", linha[0])
         print("Idade:", linha[1])
         print("Início:", linha[2], "\n")
-        #==== carregando listbox com os nomes dos registros =====
-        lb_registro.insert(END,linha[0])
+        # ==== carregando listbox com os nomes dos registros =====
+        lb_registro.insert(END, linha[0])
 
 
 def insdados():
     inicial1 = edit_inicial.get()
-    primeiro1 = edit_primeiro.get( "1.0", 'end' )
+    primeiro1 = edit_primeiro.get("1.0", 'end')
     idade1 = edit_idade.get()
     dataInicio = edit_data.get()
     telefone1 = edit_telefone.get()
     hipotese1 = edit_hipotese.get()
-    outros1 = edit_outros.get( "1.0", 'end' )
+    outros1 = edit_outros.get("1.0", 'end')
 
-    # ================ GRAVANDO O REGISTRO ================================
+    # =========================================== GRAVANDO O REGISTRO =============================================
 
-    cursor.execute( 'INSERT INTO clientes(NOME,IDADE,DATA,TELEFONE, HIPOTESE,PRIMEIRO,OUTROS)VALUES(?,?,?,?,?,?,?)',
-                    (inicial1, idade1, dataInicio, telefone1, hipotese1, primeiro1, outros1) )
+    cursor.execute('INSERT INTO clientes(NOME,IDADE,DATA,TELEFONE, HIPOTESE,PRIMEIRO,OUTROS)VALUES(?,?,?,?,?,?,?)',
+                    (inicial1, idade1, dataInicio, telefone1, hipotese1, primeiro1, outros1))
 
     banco.commit()
 
-    # =============== LIMPANDO OS CAMPOS =================================
+    # =========================================== LIMPANDO OS CAMPOS ==============================================
 
-    edit_inicial.delete( 0, END )
-    edit_primeiro.delete( 1.0, END + '-1c' )
-    edit_idade.delete( 0, END )
-    edit_data.delete( 0, END )
-    edit_telefone.delete( 0, END )
-    edit_hipotese.delete( 0, END )
-    edit_outros.delete( 1.0, END + '-1c' )
+    edit_inicial.delete(0, END)
+    edit_primeiro.delete(1.0, END + '-1c')
+    edit_idade.delete(0, END)
+    edit_data.delete(0, END)
+    edit_telefone.delete(0, END)
+    edit_hipotese.delete(0, END)
+    edit_outros.delete(1.0, END + '-1c')
 
-    print( inicial1 )
-    print( idade1 )
-    print( dataInicio )
-    print( telefone1 )
-    print( hipotese1 )
-    print( primeiro1 )
-    print( outros1 )
+    print(inicial1)
+    print(idade1)
+    print(dataInicio)
+    print(telefone1)
+    print(hipotese1)
+    print(primeiro1)
+    print(outros1)
 
 
 def semComando():
-    print( '' )
+    print('')
+
+
+def preencher():
+    edit_inicial.get = ''
+    edit_idade.get = ''
+    edit_telefone.get = ''
+    edit_data.get = ''
+    edit_hipotese.get = ''
+    edit_outros.get = ''
+    edit_primeiro.get = ''
+
+def janela_principal():
+    # ============================================= CONSTRUINDO MENUS ====================================================
+
+    barraDeMenus = Menu(tela_inicial )
+    menuContatos = Menu(barraDeMenus, tearoff=0)
+    menuContatos.add_command(label='NOVO', command=ins_janela)
+    menuContatos.add_command(label='PESQUISAR', command=nova_janela)
+    menuContatos.add_command(label='DELETAR', command=semComando)
+    menuContatos.add_separator()
+    menuContatos.add_command(label='FECHAR', command=tela_inicial.quit)
+    barraDeMenus.add_cascade(label='Contatos', menu=menuContatos)
+    tela_inicial.config(menu=barraDeMenus)
+    tela_inicial.mainloop()
+
+
+def ins_janela():
+    # ============================== CRIANDO NOVA JANELA PARA MOSTRAR OS REGISTROS ================================
+
+    topins = Toplevel()
+    topins.title('Janela Inserir')
+    topins.geometry("1150x700")
+
+    # =========================================== CRIANDO CAIXA DE TEXTO =================================================
+
+    edit_inicial = Entry(topins)
+    edit_idade = Entry(topins)
+    edit_telefone = Entry(topins)
+    edit_data = Entry(topins)
+    edit_hipotese = Entry(topins)
+    edit_outros = Text(topins)
+    edit_primeiro = Text(topins)
+
+        # ===================================== MOSTRANDO CAIXA DE TEXTO E BOTÃO =============================================
+
+    '''botao_mostrar.place(x=610, y=630, width=100, height=40)
+    botao_inicial.place(x=450, y=630, width=100, height=40)'''
+    edit_inicial.place(x=20, y=80, width=400)
+    edit_idade.place(x=533, y=80, width=30)
+    edit_hipotese.place(x=100, y=180, width=930)
+    edit_primeiro.place(x=20, y=270, width=1100, height=150)
+    edit_outros.place(x=20, y=470, width=1100, height=150)
+    edit_data.place(x=933, y=80)
+    edit_telefone.place(x=717, y=80)
+    # ============================================== CRIANDO LABEL ========================================================
+
+    lcadastro = Label(topins, text='CADASTRO DE CLIENTES')
+    label_inicial = Label( topins, textvariable=final)
+    lidade = Label(topins, text='IDADE')
+    ldata_inicio = Label(topins, text='DATA DE INICIO' )
+    ltelefone = Label(topins, text='TELEFONE')
+    lhipotese = Label(topins, text='HIPÓTESE DIAGNÓSTICA')
+    lprimeiro = Label(topins, text='PRIMEIRO ATENDIMENTO')
+    loutros = Label(topins, text='OUTROS ATENDIMENTOS')
+    # =============================================== CRIANDO BOTÃO ======================================================
+
+    botao_inicial = Button(topins, text="Imprimir", command=semComando)
+    botao_mostrar = Button(topins, text='Mostrar', command=nova_janela)
+
+    # ============================================== MOSTRANDO LABEL =====================================================
+    lcadastro.place(x=490, y=10)
+    label_inicial.place(x=19, y=60)
+    lidade.place(x=530, y=60)
+    ldata_inicio.place(x=930, y=60)
+    ltelefone.place(x=710, y=60)
+    lhipotese.place(x=490, y=16)
+    lprimeiro.place(x=19, y=250)
+    loutros.place(x=19, y=450)
+
+
+    
 
 
 
 
-#=====================================================================================================================================================
-# =================== CRIANDO E APRESENTANDO A JANELA PRINCIPAL ======================================================================================================================================================
 
+
+
+# =====================================================================================================================
+# ================================== CRIANDO E APRESENTANDO A JANELA PRINCIPAL =======================================
+# =====================================================================================================================
 
 
 tela_inicial = Tk()
-tela_inicial.title( 'Janela Consulta' )
-tela_inicial.geometry( "1150x700" )
-
-
-# =========== INICIANDO AS VARIÁVEIS ANTES DE RODAR A FUNÇÃO =============
+tela_inicial.title('Janela Consulta')
+tela_inicial.geometry("1150x700")
+# ================================= INICIANDO AS VARIÁVEIS ANTES DE RODAR A FUNÇÃO ===================================
 
 inicial1 = ''
 primeiro1 = ''
@@ -85,81 +172,19 @@ dataInicio = ''
 hipotese1 = ''
 outros1 = ''
 
+# ===================================== CRIANDO BANCO DE DADOS E TABELA ===============================================
 
-# =========== CRIANDO BANCO DE DADOS E TABELA ============================
-
-banco = sqlite3.connect( 'Banco_Clientes.db' )
+banco = sqlite3.connect('Banco_Clientes.db')
 cursor = banco.cursor()
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS clientes (NOME text, IDADE integer, DATA date, TELEFONE text, HIPOTESE text, PRIMEIRO text, OUTROS text)" )
+cursor.execute("CREATE TABLE IF NOT EXISTS clientes (NOME text, IDADE integer, DATA date, TELEFONE text, HIPOTESE text, PRIMEIRO text, OUTROS text)")
 
-# =========== CRIANDO VARIACEL ===========================================
+# =========================================== CRIANDO VARIAVEL ========================================================
 
 final = StringVar()
-final.set( 'NOME COMPLETO' )
-
-# ===================== CRIANDO LABEL =================================
-
-lcadastro = Label( tela_inicial, text='CADASTRO DE CLIENTES' )
-label_inicial = Label( tela_inicial, textvariable=final )
-lidade = Label( tela_inicial, text='IDADE' )
-ldata_inicio = Label( tela_inicial, text='DATA DE INICIO' )
-ltelefone = Label( tela_inicial, text='TELEFONE' )
-lhipotese = Label( tela_inicial, text='HIPÓTESE DIAGNÓSTICA' )
-lprimeiro = Label( tela_inicial, text='PRIMEIRO ATENDIMENTO' )
-loutros = Label( tela_inicial, text='OUTROS ATENDIMENTOS' )
-
-# ===================== CRIANDO CAIXA DE TEXTO ===========================
-
-edit_inicial = Entry()
-edit_idade = Entry()
-edit_telefone = Entry()
-edit_data = Entry()
-edit_hipotese = Entry()
-edit_outros = Text()
-edit_primeiro = Text()
-
-# ===================== CRIANDO BOTÃO ===================================
-
-botao_inicial = Button( tela_inicial, text="Imprimir", command=insdados )
-botao_mostrar = Button( tela_inicial, text='Mostrar', command=impdados )
+final.set('NOME COMPLETO')
 
 
-# ===================== MOSTRANDO LABEL ==================================
 
-lcadastro.place( x=490, y=10 )
-label_inicial.place( x=19, y=60 )
-lidade.place( x=530, y=60 )
-ldata_inicio.place( x=930, y=60 )
-ltelefone.place( x=710, y=60 )
-lhipotese.place( x=490, y=160 )
-lprimeiro.place( x=19, y=250 )
-loutros.place( x=19, y=450 )
 
-# ====================== MOSTRANDO CAIXA DE TEXTO E BOTÃO =================
+janela_principal()
 
-botao_mostrar.place( x=610, y=630, width=100, height=40 )
-botao_inicial.place( x=450, y=630, width=100, height=40 )
-edit_inicial.place( x=20, y=80, width=400 )
-edit_idade.place( x=533, y=80, width=30 )
-edit_hipotese.place( x=100, y=180, width=930 )
-edit_primeiro.place( x=20, y=270, width=1100, height=150 )
-edit_outros.place( x=20, y=470, width=1100, height=150 )
-edit_data.place( x=933, y=80 )
-edit_telefone.place( x=717, y=80 )
-
-# ====================== CONSTRUINDO MENUS ================================
-
-barraDeMenus = Menu(tela_inicial )
-menuContatos = Menu(barraDeMenus, tearoff=0 )
-menuContatos.add_command(label='NOVO', command=semComando )
-menuContatos.add_command(label='PESQUISAR', command= nova_janela )
-menuContatos.add_command(label='DELETAR', command=semComando )
-menuContatos.add_separator()
-menuContatos.add_command(label='FECHAR', command=tela_inicial.quit)
-barraDeMenus.add_cascade(label='Contatos', menu=menuContatos )
-tela_inicial.config(menu=barraDeMenus )
-
-# ====================== MOSTRANDO JANELA PRINCIPAL =======================
-
-tela_inicial.mainloop()
--- Tkinter/Segunda.py
+++ Tkinter/Segunda.py
@@ -2,42 +2,43 @@ from tkinter import *
 import pandas as pd
 import sqlite3
 
+class Janelas():
 
-def semComandoPri():
-    print('')
+    def semComandoPri():
+        print('')
 
-def nova_janela():
-    top = Toplevel()
-
-
-def menu():
-    barraDeMenusPrincipal = Menu(tela_principal )
-    menuContatosPrincipal = Menu(barraDeMenusPrincipal, tearoff=0 )
-    menuContatosPrincipal.add_command(label='NOVO', command=nova_janela )
-    menuContatosPrincipal.add_command(label='PESQUISAR', command=semComandoPri )
-    menuContatosPrincipal.add_command(label='DELETAR', command=semComandoPri )
-    menuContatosPrincipal.add_separator()
-    menuContatosPrincipal.add_command(label='FECHAR', command=tela_principal.quit)
-    barraDeMenusPrincipal.add_cascade(label='Contatos', menu=menuContatosPrincipal )
-    tela_principal.config(menu=barraDeMenusPrincipal )
+    def nova_janela():
+        top = Toplevel()
 
 
-def planilhas():
-    # ============= CARREGANDO PLANILHA DE CLIENTES DO EXCEL ============
+    def menu():
+        barraDeMenusPrincipal = Menu(tela_principal )
+        menuContatosPrincipal = Menu(barraDeMenusPrincipal, tearoff=0 )
+        menuContatosPrincipal.add_command(label='NOVO', command=nova_janela )
+        menuContatosPrincipal.add_command(label='PESQUISAR', command=semComandoPri )
+        menuContatosPrincipal.add_command(label='DELETAR', command=semComandoPri )
+        menuContatosPrincipal.add_separator()
+        menuContatosPrincipal.add_command(label='FECHAR', command=tela_principal.quit)
+        barraDeMenusPrincipal.add_cascade(label='Contatos', menu=menuContatosPrincipal )
+        tela_principal.config(menu=barraDeMenusPrincipal )
 
-    clientes = pd.read_excel('C:/Users/siuit/Documents/Python/Aprendizagem/Clientes do consultório.xlsx')
 
-    nome = clientes['NOME'].fillna(' ')
-    idade_clientes = clientes['IDADE'].fillna(' ')
-    sexo = clientes['SEXO'].fillna(' ')
-    telefone = clientes['TELEFONE'].fillna(' ')
-    bairro = clientes['BAIRRO'].fillna(' ')
-    queixainicial = clientes['QUEIXA INICIAL'].fillna(' ')
+    def planilhas():
+        # ============= CARREGANDO PLANILHA DE CLIENTES DO EXCEL ============
+
+        clientes = pd.read_excel('C:/Users/siuit/Documents/Python/Aprendizagem/Clientes do consultório.xlsx')
+
+        nome = clientes['NOME'].fillna(' ')
+        idade_clientes = clientes['IDADE'].fillna(' ')
+        sexo = clientes['SEXO'].fillna(' ')
+        telefone = clientes['TELEFONE'].fillna(' ')
+        bairro = clientes['BAIRRO'].fillna(' ')
+        queixainicial = clientes['QUEIXA INICIAL'].fillna(' ')
 
 
-tela_principal = Tk()
-tela_principal.title('Clientes')
-tela_principal.geometry( "1150x700" )
+    tela_principal = Tk()
+    tela_principal.title('Clientes')
+    tela_principal.geometry( "1150x700" )
 
 menu()
 
