2002f4e6f4e851ab14a6089429b1e6664ea9e8dc
TeoCastro <62861504+TeoCastro@users.noreply.github.com>
2021-03-25 14:27:59 -0300

    Posicionando Label

--- Tkinter/Segunda.py
+++ Tkinter/Segunda.py
@@ -11,6 +11,8 @@ class Janelas():
         self.tela()
         self.frame_tela()
         self.menus()
+        self.preencher()
+        self.mostrar()
         janela.mainloop()
 
 
@@ -47,31 +49,44 @@ class Janelas():
     def preencher(self):
         #=============== CRIANDO ENTRY ====================================
 
-        self.edit_inicial = Entry(self.janela)
-        self.edit_idade = Entry(self.janela)
-        self.edit_telefone = Entry(self.janela)
-        self.edit_data = Entry(self.janela)
-        self.edit_hipotese = Entry(self.janela)
-        self.edit_outros = Text(self.janela)
-        self.edit_primeiro = Text(self.janela)
+        self.edit_inicial = Entry(self.frame_dados2)
+        self.edit_idade = Entry(self.frame_dados2)
+        self.edit_telefone = Entry(self.frame_dados2)
+        self.edit_data = Entry(self.frame_dados2)
+        self.edit_hipotese = Entry(self.frame_dados2)
+        self.edit_outros = Text(self.frame_dados2)
+        self.edit_primeiro = Text(self.frame_dados2)
 
         #=============== CRIANDO LABEL ====================================
 
-        self.lcadastro = Label(self.janela, text='CADASTRO DE CLIENTES')
-        self.label_inicial = Label( self.janela, textvariable=final)
-        self.lidade = Label(self.janela, text='IDADE')
-        self.ldata_inicio = Label(self.janela, text='DATA DE INICIO' )
-        self.ltelefone = Label(self.janela, text='TELEFONE')
-        self.lhipotese = Label(self.janela, text='HIPÓTESE DIAGNÓSTICA')
-        self.lprimeiro = Label(self.janela, text='PRIMEIRO ATENDIMENTO')
-        self.loutros = Label(self.janela, text='OUTROS ATENDIMENTOS')
+        self.lcadastro = Label(self.frame_dados2, text='CADASTRO DE CLIENTES')
+        self.label_inicial = Label( self.frame_dados2, text='NOME')
+        self.lidade = Label(self.frame_dados2, text='IDADE')
+        self.ldata_inicio = Label(self.frame_dados2, text='DATA DE INICIO' )
+        self.ltelefone = Label(self.frame_dados2, text='TELEFONE')
+        self.lhipotese = Label(self.frame_dados2, text='HIPÓTESE DIAGNÓSTICA')
+        self.lprimeiro = Label(self.frame_dados2, text='PRIMEIRO ATENDIMENTO')
+        self.loutros = Label(self.frame_dados2, text='OUTROS ATENDIMENTOS')
 
-        # ================================ CRIANDO BOTÃO ===========================
+        # ======================= CRIANDO BOTÃO ===========================
 
         self.botao_inicial = Button(self.janela, text='Salvar', command=self.semComando)
         self.botao_mostrar = Button(self.janela, text='Mostrar', command=self.semComando)
 
         
+
+    def mostrar(self):
+        # ===================================== MOSTRANDO CAIXA DE TEXTO E BOTÃO =============================================
+
+        self.botao_mostrar.place(x=750, y=630, width=100, height=40)
+        self.botao_inicial.place(x=550, y=630, width=100, height=40)
+        self.edit_inicial.place(x=40, y=40, width=400)
+        self.edit_idade.place(x=500, y=40, width=30)
+        self.edit_hipotese.place(x=180, y=100, width=930)
+        self.edit_primeiro.place(x=20, y=150, width=1200, height=100)
+        self.edit_outros.place(x=20, y=280, width=1200, height=120)
+        self.edit_data.place(x=933, y=40)
+        self.edit_telefone.place(x=717, y=40)
 
 
 
